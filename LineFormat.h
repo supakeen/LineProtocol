ba96076a7db012fa900f9f9038a3221cc2137d5c
supakeen <cmdr@supakeen.com>
2020-11-19 22:25:47 +0100

    Initial layout?

---
+++ LineFormat.h
@@ -0,0 +1,1 @@
+#include "src/LineFormat.h"
---
+++ src/LineFormat.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef __cplusplus
+
+#include "LineFormat.hpp"
+
+using namespace LineFormat;
+
+#else
+
+#error LineFormat requires a C++ compiler, please change file extension to .cc or .cpp
+
+#endif
