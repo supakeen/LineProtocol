dcca21ed44a1e4acfb4eeee7c14cb74e9da0965a
supakeen <cmdr@supakeen.com>
2020-11-23 18:20:42 +0100

    THings.

--- src/LineProtocol.hpp
+++ src/LineProtocol.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
+#include <Arduino.h>
+
 namespace LineProtocol {
     struct line_protocol {
     };
 
-    int line_protocol_format(struct line_protocol *line_protocol) {
+    int line_protocol_format(struct line_protocol *line_protocol, String &data) {
         return 0;
     }
 
-    int line_protocol_parse(struct line_protocol *line_protocol, char *data, size_t data_size) {
+    int line_protocol_parse(struct line_protocol *line_protocol, String &data) {
         return 0;
     }
 }
