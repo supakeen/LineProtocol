4a11720b290c91fb532ae40f653abf7011e70b46
supakeen <cmdr@supakeen.com>
2021-04-25 12:36:17 +0200

    Fully class-ify.

--- test/test_lineprotocol.cpp
+++ test/test_lineprotocol.cpp
@@ -3,100 +3,83 @@
 
 #include <LineProtocol.h>
 
+void test_message_from_arguments_0(void) {
+  std::map<String, String> fields = {{"field", "field"}};
 
-void test_line_protocol_parse_0(void) {
-    struct line_protocol lp;
-    int result; 
+  std::map<String, String> tags = {{"tag", "tag"}};
 
-    result = line_protocol_parse(lp, "measurement foo=bar");
+  Message message("measurement", fields, tags, 3);
 
-    TEST_ASSERT_EQUAL(result, 0);
-    TEST_ASSERT_EQUAL_STRING("measurement", lp.measurement.c_str());
+  TEST_ASSERT_EQUAL_STRING(message.measurement.c_str(), "measurement");
+  TEST_ASSERT_EQUAL_STRING(message.fields["field"].c_str(), "field");
+  TEST_ASSERT_EQUAL_STRING(message.tags["tag"].c_str(), "tag");
+  TEST_ASSERT_EQUAL(message.timestamp, 3);
 }
 
-void test_line_protocol_parse_1(void) {
-    struct line_protocol lp;
-    int result; 
+void test_message_from_string_0(void) {
+  Message message("measurement foo=bar");
 
-    result = line_protocol_parse(lp, "test foo=bar");
-
-    TEST_ASSERT_EQUAL(result, 0);
-    TEST_ASSERT_EQUAL_STRING("test", lp.measurement.c_str());
+  TEST_ASSERT_EQUAL_STRING(message.measurement.c_str(), "measurement");
 }
 
-void test_line_protocol_parse_2(void) {
-    struct line_protocol lp;
-    int result; 
+void test_message_from_string_1(void) {
+  Message message("test,bar=foo foo=bar");
 
-    result = line_protocol_parse(lp, "test,bar=foo foo=bar");
-
-    TEST_ASSERT_EQUAL(result, 0);
-    TEST_ASSERT_EQUAL_STRING("test", lp.measurement.c_str());
-    TEST_ASSERT_EQUAL_STRING("foo",  lp.tags["bar"].c_str());
-    TEST_ASSERT_EQUAL_STRING("bar", lp.fields["foo"].c_str());
+  TEST_ASSERT_EQUAL_STRING(message.measurement.c_str(), "test");
+  TEST_ASSERT_EQUAL_STRING(message.tags["bar"].c_str(), "foo");
+  TEST_ASSERT_EQUAL_STRING(message.fields["foo"].c_str(), "bar");
 }
 
-void test_line_protocol_parse_3(void) {
-    struct line_protocol lp;
-    int result; 
-
-    result = line_protocol_parse(lp, "test,bar= foo foo=bar");
-
-    TEST_ASSERT_EQUAL(result, -1);
+void test_message_from_string_2(void) {
+  try {
+    Message message("test,bar= foo foo=bar");
+    TEST_FAIL();
+  } catch (...) {
+  };
 }
 
-void test_line_protocol_parse_4(void) {
-    struct line_protocol lp;
-    int result; 
-
-    result = line_protocol_parse(lp, "test,bar=foo fo o=bar");
-
-    TEST_ASSERT_EQUAL(result, -1);
+void test_message_from_string_3(void) {
+  try {
+    Message message("test,bar=foo fo o=bar");
+    TEST_FAIL();
+  } catch (...) {
+  };
 }
 
-void test_line_protocol_parse_5(void) {
-    struct line_protocol lp;
-    int result; 
-
-    result = line_protocol_parse(lp, ",bar=foo foo=bar");
-
-    TEST_ASSERT_EQUAL(result, -1);
+void test_message_from_string_4(void) {
+  try {
+    Message message(",bar=foo foo=bar");
+    TEST_FAIL();
+  } catch (...) {
+  };
 }
 
-void test_line_protocol_validate_0(void) {
-    struct line_protocol lp;
-    int result; 
+void test_message_validate_0(void) {
+  Message message("test,bar=foo foo=bar");
 
-    result = line_protocol_parse(lp, "test,bar=foo foo=bar");
-
-    TEST_ASSERT_EQUAL(result, 0);
-
-    TEST_ASSERT_EQUAL(0, line_protocol_validate(lp, { "bar" }, { "foo" }));
-    TEST_ASSERT_EQUAL(1, line_protocol_validate(lp, { "foo" }, { "bar" }));
-    TEST_ASSERT_EQUAL(0, line_protocol_validate(lp, { }, { }));
+  TEST_ASSERT_EQUAL(0, message.validate({"bar"}, {"foo"}));
+  TEST_ASSERT_EQUAL(1, message.validate({"foo"}, {"bar"}));
+  TEST_ASSERT_EQUAL(0, message.validate({}, {}));
 }
-
 
 void setup() {
-    UNITY_BEGIN();
+  UNITY_BEGIN();
 
-    RUN_TEST(test_line_protocol_parse_0);
-    RUN_TEST(test_line_protocol_parse_1);
-    RUN_TEST(test_line_protocol_parse_2);
-    RUN_TEST(test_line_protocol_parse_3);
-    RUN_TEST(test_line_protocol_parse_4);
-    RUN_TEST(test_line_protocol_parse_5);
+  RUN_TEST(test_message_from_arguments_0);
+  RUN_TEST(test_message_from_string_0);
+  RUN_TEST(test_message_from_string_1);
+  RUN_TEST(test_message_from_string_2);
+  RUN_TEST(test_message_from_string_3);
+  RUN_TEST(test_message_from_string_4);
 
-    RUN_TEST(test_line_protocol_validate_0);
+  RUN_TEST(test_message_validate_0);
 }
 
-void loop() {
-    UNITY_END();
-}
+void loop() { UNITY_END(); }
 
 int main() {
-    setup();
-    loop();
+  setup();
+  loop();
 
-    return 0;
+  return 0;
 }
